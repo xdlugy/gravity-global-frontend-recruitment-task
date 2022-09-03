22e2d84c4bad4fb62d929c32a5cffd78b691ac74
Kacper <xdlugy@gmail.com>
2022-09-03 18:27:21 +0200

    new gulpfile and dependencies, browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,24 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-var replace = require('gulp-replace');
-
-// File paths
-const files = { 
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
-};
-
-function scssTask(){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
 }
 
-function jsTask(){
-    return src([
-        files.jsPath
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(dest('dist')
-    );
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('*.html').on('change',browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 }
 
-var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
-}
-
-function watchTask(){
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));    
-}
-
-exports.default = series(
-    parallel(scssTask, jsTask), 
-    cacheBustTask,
-    watchTask
-);
+exports.style = style;
+exports.watch = watch;
