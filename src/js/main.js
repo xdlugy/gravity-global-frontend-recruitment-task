c206127897f21ffcf2462d0ff87d92b3b2aefd30
Kacper <xdlugy@gmail.com>
2022-09-04 22:25:27 +0200

    popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
-function popup(){
-    document.getElementById("over").style.display="block";
-    document.getElementById("popup");
-}
+document.cookie="0";
+window.onload = function(){
+
+    const over = document.getElementById('over');
+    const button = document.getElementById('button');
+    const popup = document.getElementById('popup');
+    const close = document.getElementById('close');
+    var licznik=document.cookie;
+
+    button.onclick = function() {
+        over.style.display="block";
+        licznik++;
+        popup.innerHTML=licznik;
+        document.cookie=licznik;
+    }
+
+    window.onclick = function(event) {
+        if (event.target == over) {
+          over.style.display = "none";
+        }
+    }
+
+    close.onclick= function(){
+        over.style.display="none";
+    }
+};
