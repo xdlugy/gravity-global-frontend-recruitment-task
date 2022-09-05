eb7bfd3a28176127a3a7909d522d2eb336ac6aad
Kacper <xdlugy@gmail.com>
2022-09-05 20:30:03 +0200

    transitions

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,42 @@ window.onload = function(){
     const button = document.getElementById('button');
     const popup = document.getElementById('popup');
     const close = document.getElementById('close');
+    const bold = document.getElementsByTagName('b');
+    const reset = document.getElementById('reset');
 
     var licznik=document.cookie;
 
     button.onclick = function() {
-        over.style.display="block";
+        over.style.visibility="visible";
+        over.style.backgroundColor="rgba(0,0,0,0.4)";
+        popup.style.top="50%";
         licznik++;
-        popup.innerHTML=licznik;
+        bold[0].innerHTML=licznik+" times";
         document.cookie=licznik;
+        if(document.cookie>5)
+            reset.style.visibility="visible";
+            else
+            reset.style.visibility="hidden";
+    }
+
+    reset.onclick = function() {
+        document.cookie=0;
+        licznik=0;
+        bold[0].innerHTML="0 times";
+        reset.style.visibility="hidden";
     }
 
     window.onclick = function(event) {
         if (event.target == over) {
-          over.style.display = "none";
+          over.style.visibility = "hidden";
+          popup.style.top="-20%";
+          over.style.backgroundColor="rgba(0,0,0,0)";
         }
     }
 
     close.onclick= function(){
-        over.style.display="none";
+        over.style.visibility="hidden";
+        popup.style.top="-20%";
+          over.style.backgroundColor="rgba(0,0,0,0)";
     }
 };
